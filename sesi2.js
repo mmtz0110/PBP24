37fa03936a6db6e09dddf48e738ead00f038dfd7
mmtz0110 <yarmgamingch@gmail.com>
2025-09-22 16:36:43 +0700

    rename to hello.js

---
+++ sesi2.js
@@ -0,0 +1,10 @@
+var fName = "John";
+
+function printFullName(){
+    var lName = "Doe";
+    return fName + " " + lName;
+}
+
+console.log(printFullName());
+console.log(fName);
+console.log(lName);
